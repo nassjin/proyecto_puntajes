b744105fb8b0d31c15a5af09611ba4d5a2616798
Nassjin <ns.devcoding@gmail.com>
2025-09-03 16:49:05 -0400

    add configuration export_excel tables

--- export_excel.py
+++ export_excel.py
@@ -35,11 +35,34 @@ def export_excel(records: list[dict], filename: str = "puntajes_export.xlsx") ->
         "Anot. Leves 2", "Anot. Graves 2", "Anot. Gravísimas 2",
         "Prom. Lneguaje 1", "Prom. Lenguaje 2",
         "Prom. Matemática 1", "Prom. Matemática 2",
+        "Prom. General 1", "Prom. General 2",
+        "Prom.Tecno 1", "Prom.Tecno 2",
+        "SIMCE Lenguaje 2", "SIMCE Matemática 2",
+        "Asistencia 1", "Asistencia 2",
+        "Puntaje Entrevistas"
 
     ]
     ws_detalle.append(headers_detalle)
 
     # Estilo encabezado detalle
+    for cell in ws_detalle[1]:
+        cell.font = Font(bold=True, color="FFFFFF")
+        cell.fill = PatternFill(start_color="9BBB59", end_color="9BBB59", fill_type='solid')
+
+
+    #Rellenar datos
+    for record in records:
+        puntajes = calcular_puntaje_total(record)
+
+        #fila de resultados
+        ws_resultado.append([
+            record.get("rut"), record.get("full_name"), record.get("course"),
+            record.get("eleccion_1"), record.get("eleccion_2"), record.get("eleccion_3"),
+            record.get("eleccion_4"), record.get("eleccion_5"),
+            puntajes["anotaciones"], puntajes["notas_base"], puntajes["tecnologia"], puntajes["simce"], puntajes["asistencia"],
+            puntajes["entrevistas"], puntajes["total_final_puntajes"]
+        ])
+
 
 
 
